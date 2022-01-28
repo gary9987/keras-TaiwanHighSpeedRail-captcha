741daca344856082702bac2642b36bbdb616c44a
Gary <gary369880206@gmail.com>
2022-01-28 20:46:05 +0800

    fix fetch img by add header

--- src/download_img/get_img.py
+++ src/download_img/get_img.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
-import time
-import urllib.request
 import requests
+from pathlib import Path
 
 if __name__ == '__main__':
+
+    Path("./image").mkdir(parents=True, exist_ok=True)
     count = 0
     session = requests.Session()
+    headers = {'user-agent': 'Mozilla/5.0'}
     while count <= 5000:
         count += 1
-        response = session.get('https://irs.thsrc.com.tw/IMINT/', cookies={'from-my': 'browser'})
+        response = session.get('https://irs.thsrc.com.tw/IMINT/', headers=headers, cookies={'from-my': 'browser'})
         source = response.content.decode('utf-8')
         soup = BeautifulSoup(source, 'html.parser')
         img = soup.find('img')
         url = 'https://irs.thsrc.com.tw'+img.get('src')
         print(url)
-        response = session.get(url, cookies={'from-my': 'browser'})
-        with open('/image/'+str(count)+'.jpg', 'wb') as file:
+        response = session.get(url, headers=headers, cookies={'from-my': 'browser'})
+        with open('./image/'+str(count)+'.jpg', 'wb') as file:
             file.write(response.content)
             file.flush()
